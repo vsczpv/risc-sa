332ee4077c2d1b62fbe595ad78204ca98b063966
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2023-08-07 15:06:58 -0300

    Initial commit
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <rsa.hpp>
+
+auto rsa::main(int argc, char* argv[]) -> int
+{
+
+	(void) argc, (void) argv;
+
+	std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
---
+++ rsa.hpp
@@ -0,0 +1,9 @@
+#ifndef RSA_RSA_HPP_
+#define RSA_RSA_HPP_
+
+namespace rsa
+{
+	extern "C" auto main(int, char**) -> int;
+}
+
+#endif // RSA_RSA_HPP_

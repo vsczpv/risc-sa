40621a12d02cdc515fba27eb78adaba3803f4fbc
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2023-08-17 16:36:46 -0300

    Finish state machine
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- app.cpp
+++ app.cpp
@@ -5,6 +5,7 @@
 
 #include <optional>
 #include <string>
+#include <cstring>
 
 using namespace rsa;
 
@@ -17,35 +18,70 @@ auto parse_cmdline(int argc, char* argv[]) -> std::optional <std::string>
 	/*
 	 * risc-sa -o ORG PRG.txt
 	 */
-	if (argc < 4) return "Not enough arguments";
+//	if (argc < 4) return "Not enough arguments";
 
 	enum CmdState
 	{
-		OnProgramExpect,
-		OnDashArgument,
+		OnDashAndProgramExpect,
 		OnOrgExtract
 	}
-	state = OnProgramExpect;
+	state = OnDashAndProgramExpect;
 
 	bool running = true;
 	int  index   = 1;
 
+	int  org_id = 0;
+	bool has_a_program   = false;
 
 	while (running)
 	{
+
+		auto& text = argv[index];
+
 		switch (state)
 		{
-			case OnProgramExpect:
+			case OnDashAndProgramExpect:
+			{
+				if (std::strcmp(text, "-o") == 0)
+				{
+					state = OnOrgExtract;
+					break;
+				}
+
+				else if (text[0] == '-') return "Invalid argument";
+
+				else
+				{
+					if (has_a_program)         return "Can only specify one program";
+
+					has_a_program = true;
+					rsa::program  = rv::program(text);
+
+					if (!program.has_opened()) return "Could not open specified program";
+				}
+			}
+			break;
+
+			case OnOrgExtract:
 			{
 
+				rsa::organizations.push_back
+				(
+					rsa::rv::organization (std::string(text), org_id+1)
+				);
+				if (!rsa::organizations[org_id].is_valid()) return "Could not parse specified organization.";
+
+				org_id++, state = OnDashAndProgramExpect;
 			}
-			case OnDashArgument:
-			case OnOrgExtract:
-				running = false;
-				break;
+			break;
 		}
-		break;
+
+		index++;
+		if (index == argc) break;
 	}
+
+	if (has_a_program == false) return "You must specify a program.";
+	if (org_id == 0)            return "You must specify atleast one organization.";
 
 	return std::nullopt;
 }
